23ad9be5929ca6e17bee0004cc691057b1e80625
abdo <abdelsalem.yabrag@hotmail.com>
2019-07-22 15:12:01 +0200

    ida2lab2d4

--- src/textproc/BookReaderController.java
+++ src/textproc/BookReaderController.java
@@ -1,9 +1,15 @@
 package textproc;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.util.*;
 
 public class BookReaderController extends Application {
     @Override
@@ -14,6 +20,35 @@ public class BookReaderController extends Application {
         primaryStage.setTitle("Book Reader");
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        List<TextProcessor> textProcessors = new ArrayList<>();
+        Scanner scanner = new Scanner(new File("src/undantagsord.txt"));
+        scanner.useDelimiter("(\\s|,|\\.|:|;|!|\\?|'|\\\")+");
+        Set<String> stopWords = new HashSet<>();
+        while (scanner.hasNext()) {
+            stopWords.add(scanner.next().toLowerCase());
+        }
+        GeneralWordCounter generalWordCounter = new GeneralWordCounter(stopWords);
+        textProcessors.add(generalWordCounter);
+
+        Scanner s = null;
+        for (TextProcessor textProcessor : textProcessors) {
+            s = new Scanner(new File("src/nilsholg.txt"));
+            s.useDelimiter("(\\s|,|\\.|:|;|!|\\?|'|\\\")+"); // se handledning
+
+            while (s.hasNext()) {
+                String word = s.next().toLowerCase();
+
+                textProcessor.process(word);
+            }
+//                textProcessor.report();
+        }
+        s.close();
+
+        ObservableList<Map.Entry<String, Integer>> words = FXCollections.observableArrayList(generalWordCounter.getWords());
+        ListView<Map.Entry<String, Integer>> listView = new ListView<>(words);
+        root.setCenter(listView);
+
     }
 
     public static void main(String[] args) {
