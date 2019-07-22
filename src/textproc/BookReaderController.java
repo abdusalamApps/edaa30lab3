c32ef418ac46d5832b4afe7b122b3a49b0f0cdb8
abdo <abdelsalem.yabrag@hotmail.com>
2019-07-22 14:39:35 +0200

    ida2lab2d2

---
+++ src/textproc/BookReaderController.java
@@ -0,0 +1,15 @@
+package textproc;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class BookReaderController extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
